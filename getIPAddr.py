a197c7ad29eb13be0558429684959c4db74fb95a
jacklam718 <jacklam718@gmail.com>
2014-03-19 23:33:07 +0800

    get current ip address

---
+++ getIPAddr.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import socket 
+
+def getIP( ):
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(('www.google.com', 80))
+	addr = s.getsockname( )[0]  
+	s.close( )  
+	return addr 
+
+
+address = ( getIP( ), 5000 )   
+
